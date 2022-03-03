c8b6f41751b43c97f368f0cc08711609e2b79651
Ryan Fleharty <ryan.fleharty@gmail.com>
2022-03-02 17:18:05 -0700

    hamburger on mobile might be working?

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 $('#top-nav>ul>li').on('mouseenter mouseleave', function(e){
     $(this).children('.top-nav-dropdown-menu').toggleClass("active")
 })
+
+$(".burger-menu").on("click", function(){
+    $(this).toggleClass("change");
+    $("#top-nav-dropdown-parent-list").toggleClass("active")
+})
